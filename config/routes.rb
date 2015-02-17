b7864e17850b04117f53d9fcd09a60fbd6e2a3e2
ROEL CANTU <roel.cantu@yahoo.com>
2015-02-17 13:32:07 -0600

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+  get 'users/new'
+
+  root 'users#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
